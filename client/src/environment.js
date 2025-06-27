eb742dabad34e9174172b46d2e96e897ed9cd693
Rakibul Islam <rakibulislammondal21@gmail.com>
2025-06-28 00:17:16 +0530

    Updated environment.js

--- client/src/environment.js
+++ client/src/environment.js
@@ -1,6 +1,5 @@
-// const IS_PROD = false;
-// const server =   IS_PROD ? "https://alexithymia-backend.onrender.com" : "http://localhost:8800";
+const IS_PROD = true;
+const server =   IS_PROD ? "https://alexithymia-backend.onrender.com" : "http://localhost:8800";
 
-const server = "http://localhost:8800";
 
 export default server;
