921659a0556575c6bd6a2ab2594fa3bc49d7e211
Rakibul Islam Mondal <110806922+Rakibul440@users.noreply.github.com>
2025-06-28 10:55:30 +0530

    Update UpdateForm.jsx

--- client/src/components/update/updateForm/UpdateForm.jsx
+++ client/src/components/update/updateForm/UpdateForm.jsx
@@ -54,7 +54,7 @@ export default function UpdateForm(props) {
   return (
     <motion.div className='update-content' initial={{opacity:0,scale:0.9}} animate={{opacity:1,scale:1,transition:{type:"spring",stiffness:50}}} >
       <form id='details' action="" >
-            <input type="text" name='title' value={""} onChange={handleChange} placeholder='Title' />
+            <input type="text" name='title' value={update.title} onChange={handleChange} placeholder='Title' />
             <textarea name='expression' value={update.expression} onChange={handleChange} placeholder='Storyline' rows="5"></textarea>
             <input type="text" name='author' value={update.author} onChange={handleChange} placeholder='Author' />
             <input type="text" name='cover' value={update.cover} onChange={handleChange} placeholder='Picture url' />
